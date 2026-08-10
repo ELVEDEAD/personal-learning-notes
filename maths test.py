7aa10928f8bba1319703af3a6442501cc9fe0629
ELVEDEADEdit profile <1054187887@qq.com>
2026-08-10 09:12:12 +0800

    数学实验

---
+++ maths test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1, 8)
+y = x ** 2
+plt.plot(x,y)
+plt.title("平方曲线测试")
+plt.show()
---
+++ 函数.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+a=float(input("请输入一个数字："))
+b=float(input("请输入下一个数字："))
+res=a+b
+lista=[a,b,res]
+listb=['数字1','数字2','和']
+plt.bar(listb,lista,color=['#555555',"#8A2525",'#556666'])
+plt.title("柱状图")
+plt.show()
